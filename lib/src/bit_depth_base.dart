7f1ca26f8367ded49af291e7cb70babfc45f6f88
Mason199004 <mason1990haines@gmail.com>
2021-01-05 22:30:49 -0600

    allow for exceptions to be thrown on overflow and underflow //TODO: add unsigned types

--- lib/src/bit_depth_base.dart
+++ lib/src/bit_depth_base.dart
@@ -6,19 +6,57 @@ import 'dart:typed_data';
 ///Base Number Class
 abstract class Number
 {
+  ///Determines whether an exception should be thrown on overflow and underflow
+  static bool should_throw = false;
   ByteData _data;
   dynamic GetValue();
 
 }
+
+class IntegerException implements Exception
+{
+  String _message;
+
+  IntegerException([String message = 'Invalid value']) {
+    this._message = message;
+  }
+
+  @override
+  String toString() {
+    return _message;
+  }
+}
+
+class IntegerOverflowException extends IntegerException
+{
+  IntegerOverflowException(String message)
+  {
+    _message = message;
+  }
+}
+
+class IntegerUnderflowException extends IntegerException
+{
+  IntegerUnderflowException(String message)
+  {
+    _message = message;
+  }
+}
+
 ///8 bit Signed Integer
 class Int8_t extends Number
 {
   Int8_t(int val)
   {
+    // ignore: prefer_single_quotes
+    if (Number.should_throw && (val > 127)) {throw IntegerOverflowException("${val} is greater than 8 bits!");}
+    if (Number.should_throw && (val < -128)) {throw IntegerUnderflowException("${val} is greater than 8 bits!");}
     _data = new ByteData(1);
     _data.setInt8(0, val);
   }
   void SetValue(int val) {
+    if (Number.should_throw && (val > 127)) {throw IntegerOverflowException("${val} is greater than 8 bits!");}
+    if (Number.should_throw && (val < -128)) {throw IntegerUnderflowException("${val} is greater than 8 bits!");}
     _data.setInt8(0, val);
   }
 
@@ -32,10 +70,14 @@ class Int16_t extends Number
 {
   Int16_t(int val)
   {
+    if (Number.should_throw && (val > 32767)) {throw IntegerOverflowException("${val} is greater than 16 bits!");}
+    if (Number.should_throw && (val < -32768)) {throw IntegerUnderflowException("${val} is greater than 16 bits!");}
     _data = new ByteData(2);
     _data.setInt16(0, val);
   }
   void SetValue(int val) {
+    if (Number.should_throw && (val > 32767)) {throw IntegerOverflowException("${val} is greater than 16 bits!");}
+    if (Number.should_throw && (val < -32768)) {throw IntegerUnderflowException("${val} is greater than 16 bits!");}
     _data.setInt16(0, val);
   }
 
@@ -47,12 +89,18 @@ class Int16_t extends Number
 ///32 bit Signed Integer
 class Int32_t extends Number
 {
-  Int32(int val)
+
+
+  Int32_t(int val)
   {
+    if (Number.should_throw && (val > 2147483647)) {throw IntegerOverflowException("${val} is greater than 32 bits!");}
+    if (Number.should_throw && (val < -2147483648)) {throw IntegerUnderflowException("${val} is greater than 32 bits!");}
     _data = new ByteData(4);
     _data.setInt32(0, val);
   }
   void SetValue(int val) {
+    if (Number.should_throw && (val > 2147483647)) {throw IntegerOverflowException("${val} is greater than 32 bits!");}
+    if (Number.should_throw && (val < -2147483648)) {throw IntegerUnderflowException("${val} is greater than 32 bits!");}
     _data.setInt32(0, val);
   }
 
@@ -66,10 +114,14 @@ class Int64_t extends Number
 {
   Int64_t(int val)
   {
+    if (Number.should_throw && (val > 9223372036854775807)) {throw IntegerOverflowException("${val} is greater than 64 bits!");}
+    if (Number.should_throw && (val < -9223372036854775808)) {throw IntegerUnderflowException("${val} is greater than 64 bits!");}
     _data = new ByteData(8);
     _data.setInt64(0, val);
   }
   void SetValue(int val) {
+    if (Number.should_throw && (val > 9223372036854775807)) {throw IntegerOverflowException("${val} is greater than 64 bits!");}
+    if (Number.should_throw && (val < -9223372036854775808)) {throw IntegerUnderflowException("${val} is greater than 64 bits!");}
     _data.setInt64(0, val);
   }
 
--- test/bit_depth_test.dart
+++ test/bit_depth_test.dart
@@ -10,6 +10,20 @@ void main() {
     });
 
     test('First Test', () {
+        assert(Int8_t(127).GetValue() == 127);
+        assert(Int16_t(32767).GetValue() == 32767);
+        assert(Int32_t(2147483647).GetValue() == 2147483647);
+        assert(Int64_t(9223372036854775807).GetValue() == 9223372036854775807);
+        assert(Int64_t(7) is Number);
+        Number.should_throw = true;
+        assert(Int8_t(127).GetValue() == 127);
+        assert(Int16_t(32767).GetValue() == 32767);
+        assert(Int32_t(2147483647).GetValue() == 2147483647);
+        assert(Int64_t(9223372036854775807).GetValue() == 9223372036854775807);
+        assert(Int64_t(7) is Number);
+
+        expect(() => Int8_t(255), throwsA(TypeMatcher<IntegerOverflowException>()));
+        expect(() => Int8_t(-255), throwsA(TypeMatcher<IntegerUnderflowException>()));
 
     });
   });
